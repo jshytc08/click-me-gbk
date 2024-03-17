06770a4c09eb82f063b0f8edee33d526247ee707
Joshua <ytacjoshua@gmail.com>
2024-03-17 12:52:46 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 const answers_yes = {
     english: [
-        "No",
-        "Are you sure?",
-        "Are you really sure??",
-        "Are you really realy sure???",
-        "Think again?",
-        "Don't believe in second chances?",
-        "Why are you being so cold?",
-        "Maybe we can talk about it?",
-        "I am not going to ask again!",
-        "Ok now this is hurting my feelings!",
-        "You are now just being mean!",
-        "Why are you doing this to me?",
-        "Please give me a chance!",
-        "I am begging you to stop!",
+        "Sorry hon.",
+        "Sorry na po please.",
+        "Wag ka na magalit mahal.",
+        "Please hon.",
+        "Don't be mad at me.",
+        "I'm sorry honey.",
+        "I love you so much.",
+        "Please don't be mad at me anymore.",
+        "I'm sorry my love.",
+        "Sorry sorry sorry sorry sorry",
+        "Sorry honey.",
+        "I'm super sorry my love.",
+        "Please accept my apology",
+        "I love you hon.",
         "Ok, Let's just start over.."
     ]
 };
 
 answers_no = {
-    "english": "Yehey"
+    "english": "No"
 }
 
 let language = "english"; // Default language is English
